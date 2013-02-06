581dfb03c2e2d6ae0a14c3c9b3b59d0c80e207c3
Michael Puterbaugh <mputerba@gmail.com>
2013-02-05 23:12:56 -0500

    Initial Commit

---
+++ reversebinary/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  reversebinary
+//
+//  Created by Michael Puterbaugh on 2/5/13.
+//  Copyright (c) 2013 Michael Puterbaugh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
